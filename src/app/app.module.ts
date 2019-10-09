070c2e7ccdabd66e047338dd8df5657cf9ddecfa
JavierMarAba <javier.martinezabarca@nunegal.com>
2019-10-09 17:55:23 +0200

    'pub'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
-@NgModule({
+
+@NgModule( {
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
-})
+} )
 export class AppModule { }
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from '../app-routing.module';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { LayoutComponent } from './layout/layout.component';
+import { MainComponent } from './layout/main/main.component';
+
+
+
+
+@NgModule( {
+  declarations: [LayoutComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule
+  ],
+  exports: [
+    LayoutComponent
+  ]
+} )
+export class CoreModule { }
